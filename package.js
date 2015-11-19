42df778b25051b404b430797272e43e10695310c
David Collado Sela <bitomule@gmail.com>
2015-11-19 15:12:55 +0100

    bind on register error

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "mmmelon:sns-push",
   summary: "sns-push makes sending and managing push notifications easy. Uses Amazon sns.",
-  version: "0.2.1",
+  version: "0.2.2",
   git: "https://github.com/mmmelon/sns-push"
 });
 
